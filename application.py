2b38a7fd001d0ebe1a573dd88114534b9831418e
Marcus Merkel <marcus@marcusmerkel.de>
2020-05-17 16:40:38 +0200

    typo fix

--- application.py
+++ application.py
@@ -48,7 +48,7 @@ def login_required(f):
     @wraps(f)
     def decorated_function(*args, **kwargs):
         if session.get("user_id") is None:
-            return redi rect("/login")
+            return redirect("/login")
         return f(*args, **kwargs)
     return decorated_function
 
